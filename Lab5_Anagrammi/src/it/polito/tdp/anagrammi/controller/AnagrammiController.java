e29f191f0314797e47d1c1849f4443ff345ca31c
Giulia <Giulia@172.21.56.27>
2019-04-03 15:24:26 +0200

    Implementato l'intefaccia

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
@@ -1,13 +1,63 @@
+
 package it.polito.tdp.anagrammi.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import it.polito.tdp.anagrammiModel.AnagrammiModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AnagrammiController {
-
-private AnagrammiModel model;
+	
+	private AnagrammiModel model;
 	
 	public void setModel(AnagrammiModel model) {
 		this.model = model;
 	}
-}
+	
+    @FXML
+    private ResourceBundle resources;
 
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtInserisci;
+
+    @FXML
+    private Button btmAnagramma;
+
+    @FXML
+    private TextArea txtParoleCorrette;
+
+    @FXML
+    private TextArea txtParoleErrate;
+    
+
+    @FXML
+    private Button btmReset;
+
+    @FXML
+    void doAnagramma(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtInserisci != null : "fx:id=\"txtInserisci\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btmAnagramma != null : "fx:id=\"btmAnagramma\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtParoleCorrette != null : "fx:id=\"txtParoleCorrette\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtParoleErrate != null : "fx:id=\"txtParoleErrate\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btmReset != null : "fx:id=\"btmReset\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+
+    }
+}
